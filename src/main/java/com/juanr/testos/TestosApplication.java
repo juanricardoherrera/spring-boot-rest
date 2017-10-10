6dd5243e8ffcc5c1be55a086be63ee0f4b2102f8
juan.herrera <juan.herrera@pragma.com.co>
2017-10-10 17:07:43 -0500

    Ajuste servicio saludo

--- src/main/java/com/juanr/testos/TestosApplication.java
+++ src/main/java/com/juanr/testos/TestosApplication.java
@@ -2,8 +2,8 @@ package com.juanr.testos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -15,7 +15,7 @@ public class TestosApplication {
 	}
         
         @RequestMapping("/hello/{nombre}")
-        public String hello(@RequestParam String nombre){
+        public String hello(@PathVariable String nombre){
             return "Hello: " + nombre;
         }
 }

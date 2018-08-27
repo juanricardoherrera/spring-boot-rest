57def76a24cc6c27d6073d49c79c35417de7620a
juan.herrera <juan.herrera@pragma.com.co>
2018-08-27 05:39:38 -0500

    Cambio para tomar variable desde YAML

--- src/main/java/com/juanr/testos/TestosApplication.java
+++ src/main/java/com/juanr/testos/TestosApplication.java
@@ -2,16 +2,21 @@ package com.juanr.testos;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, CassandraDataAutoConfiguration.class})
 public class TestosApplication {
 
-	@Value("${spring.variable-opensfhit}") String variableOpenshiftTest;
+	@Value("${openshift.variable}") String variableOpenshiftTest;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TestosApplication.class, args);
